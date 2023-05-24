dd5d190724c3d90fd16809469136d2188fb446fa
Yuuki Reiya <yuukibamba@gmail.com>
2023-05-25 00:53:45 +0900

    develop メインループ実装途中

---
+++ Docker/opt/game.py
@@ -0,0 +1,12 @@
+import pygame
+
+class Game():
+
+    def __init__(self):
+        print("Game::__init__")
+
+    def update(self):
+        print("Game::Update")
+
+    def draw(self):
+        print("Game::Draw")
